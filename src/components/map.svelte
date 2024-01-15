7e6ceaebdefec50062eac2d88a0797865bbc5504
MrSmarto <thijmensmarto@gmail.com>
2024-01-15 11:13:24 +0100

    test

--- src/components/map.svelte
+++ src/components/map.svelte
@@ -1,26 +1,6 @@
+<!-- MapComponent.svelte -->
 <script>
-    import { onMount } from 'svelte';
-    import L from 'leaflet';
-  
-    let map;
-  
-    onMount(() => {
-      map = L.map('map').setView([52.3676, 4.904
-  1], 13); // De coördinaten voor Amsterdam
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '© OpenStreetMap contributors'
-  }).addTo(map);
-  });
+  // Logica voor de Leaflet-kaart
 </script>
 
-<div id="map" style="width: 100%; height: 500px;"></div>
-
-<style>
-  #map {
-    height: 100%;
-    width: 100%;
-    position: absolute;
-    top: 0;
-    left: 0;
-  }
-</style>
+<div id="map" style="height: 660px;"></div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,33 @@
 <script>
-  import Map from "$components/Map.svelte";
+  import { onMount } from "svelte";
+
+  let map;
+
+  // Functie die uitgevoerd wordt zodra de component is gemount in de DOM
+  onMount(async () => {
+    if (typeof window !== "undefined") {
+      // Dynamisch importeren van Leaflet en de bijbehorende CSS
+      const leaflet = await import("leaflet");
+      const L = leaflet.default;
+      await import("leaflet/dist/leaflet.css");
+
+      // Initialisatie van de Leaflet-kaart
+      map = L.map("map").setView([52.1326, 5.2913], 7); // Coördinaten en zoomniveau voor Nederland
+
+      // Voeg een standaard tileLayer toe
+      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        maxZoom: 19,
+        attribution: "© OpenStreetMap contributors",
+      }).addTo(map);
+    }
+  });
 </script>
 
-```svelte
-<Map />
+<!-- De kaart container -->
+<div id="map" style="width: 100%; height: 700px;"></div>
+
+
+<!-- De styling van alle componenten -->
+<style>
+  @import "../styles/Global.css";
+</style>
