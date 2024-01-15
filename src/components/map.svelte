babc9807441f3f2c86450bd8da2fe291115d161f
MrSmarto <thijmensmarto@gmail.com>
2024-01-15 10:46:32 +0100

    test

--- src/components/map.svelte
+++ src/components/map.svelte
@@ -1,18 +1,26 @@
-<!-- src/components/Map.svelte -->
 <script>
-  import { onMount } from "svelte";
-  import L from "leaflet";
-  import "leaflet/dist/leaflet.css";
-
-  let map;
-
-  onMount(() => {
-    map = L.map("map").setView([52.3676, 4.9041], 13);
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      maxZoom: 18,
-      attribution: "© OpenStreetMap contributors",
-    }).addTo(map);
+    import { onMount } from 'svelte';
+    import L from 'leaflet';
+  
+    let map;
+  
+    onMount(() => {
+      map = L.map('map').setView([52.3676, 4.904
+  1], 13); // De coördinaten voor Amsterdam
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '© OpenStreetMap contributors'
+  }).addTo(map);
   });
 </script>
 
-<div id="map" style="height: 500px;"></div>
+<div id="map" style="width: 100%; height: 500px;"></div>
+
+<style>
+  #map {
+    height: 100%;
+    width: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,6 @@
 <script>
-  var mymap = L.map("mapid").setView([52.3676, 4.9041], 13); // Coördinaten voor Amsterdam
-
-  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    maxZoom: 18,
-    attribution: "© OpenStreetMap contributors",
-  }).addTo(mymap);
+  import Map from "$components/Map.svelte";
 </script>
+
+```svelte
+<Map />
