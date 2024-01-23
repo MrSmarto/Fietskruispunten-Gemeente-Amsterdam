e90068f2479f8b2fa257fc2a27a2ad270ae3bfbb
MrSmarto <thijmensmarto@gmail.com>
2024-01-23 14:49:57 +0100

    colors fixed

--- src/utils/geocolors.js
+++ src/utils/geocolors.js
@@ -1,23 +1,31 @@
 export const geofietspuntencolors = (layer) => {
     switch (layer) {
-      case '110_asfalt_fiets_zwart_vlak':
-        return '#8c8c8c'; // Zwart/grijs
-      case '120_asfalt_fiets_rood_vlak':
-        return '#ed7070'; // Rood
-      case '200_klinker_rood_vlak':
-        return '#ff8c00'; // Oranje
-      case '130_tegelwerk_fiets_rood_vlak':
-        return '#ed7070'; // Rood
-      case '210_tegelwerk_grijs_ vlak':
-        return '#c9c9c9'; // Grijs
-      case '300_ovbaan_zwart_vlak':
-        return '#434343'; // Donkergrijs
-      case '500_markering_wit_vlak':
-        return '#ffffff'; // Wit
-      case '50_groenvak_vlak':
-        return '#008000'; // Groen
-      default:
-        return 'none'; // Geen kleur / transparant
+		case '100_asfalt_auto_zwart_vlak':
+			return '#8c8c8c';
+		case '110_asfalt_fiets_zwart_vlak':
+			return '#8c8c8c';
+		case '120_asfalt_fiets_rood_vlak':
+			return '#ed7070';
+		case '130_tegelwerk_fiets_rood_vlak':
+			return '#ed9393';
+		case '200_klinker_rood_vlak':
+			return '#eb8b70';
+		case '210_tegelwerk_grijs_ vlak':
+			return '#c9c9c9';
+		case '25_niet_openbaar_wit_vlak':
+			return 'none';
+		case '300_ovbaan_zwart_vlak':
+			return '#8c8c8c';
+		case '450_Inritconstructie_grijs_vl':
+			return '#c9c9c9';
+		case '50_groenvak_vlak':
+			return '#9fe6a6';
+		case '500_markering_wit_vlak':
+			return '#f7f7f7';
+		case '800_bomen':
+			return '#29b000';
+		default:
+			return 'none';
     }
   };
   
