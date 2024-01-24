aa3e678a164e9630d07f242ddab0a67096e0466e
MrSmarto <thijmensmarto@gmail.com>
2024-01-25 00:49:29 +0100

    vizualisatie added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,8 @@
   import HerontworpenOverlay from "../components/HerontworpenOverlay.svelte";
   import HomeOverlay from "../components/HomeOverlay.svelte";
   import OpvallendeOverlay from "../components/OpvallendeOverlay.svelte";
+  import HerontwerpvriOverlay from "../components/HerontwerpvriOverlay.svelte"; // Zorg ervoor dat de import correct is
+  import Hoofdvizualisatie from "../components/Hoofdvizualisatie.svelte"; // Importeer de visualisatiecomponent
   import { geofietspuntencolors } from "../utils/geocolors.js";
 
   let map;
@@ -15,25 +17,21 @@
       const L = leaflet.default;
       await import("leaflet/dist/leaflet.css");
 
-      map = L.map("map", {
-        maxBounds: [
-          [52.278174, 4.728759],
-          [52.431157, 5.079162],
-        ],
-      }).setView([52.3676, 4.9041], 13);
+      // Specifieke coördinaten en zoomniveau voor de initiële weergave
+      const specifiekeLocatie = [52.37413259215145, 4.875108445719616];
+      const zoomNiveau = 20;
+
+      map = L.map("map").setView(specifiekeLocatie, zoomNiveau);
 
       L.tileLayer("https://t1.data.amsterdam.nl/topo_wm_zw/{z}/{x}/{y}.png", {
         attribution: "&copy; Gemeente Amsterdam",
-        maxZoom: 21,
+        maxZoom: 22,
       }).addTo(map);
 
+      // GeoJSON data laden
       try {
-        const response = await fetch(
-          "/data/fietskruispunten_nieuwe_situatie.geojson"
-        );
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
+        const response = await fetch("/data/fietskruispunten_nieuwe_situatie.geojson");
+        if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
         const data = await response.json();
 
         L.geoJSON(data, {
@@ -49,33 +47,24 @@
       }
     }
   });
-
-  function toggleOverlay() {
-    overlayStatus.update((n) => !n); // Toggle de status van de overlay
-  }
-
-  // Nieuwe functie om de overlay te sluiten of te veranderen
-  function handleMenuItemClick(selectedItem) {
-    if ($overlayContent === selectedItem && $overlayStatus) {
-      overlayStatus.set(false); // Sluit de overlay als hetzelfde item opnieuw wordt geklikt
-    } else {
-      overlayContent.set(selectedItem); // Verander de inhoud van de overlay
-      overlayStatus.set(true); // Open de overlay
-    }
-  }
 </script>
 
 <Navbar />
 
+<!-- Voeg de visualisatiecomponent toe binnen je conditionele overlay of ergens anders op de pagina -->
+<Hoofdvizualisatie />
+
 {#if $overlayStatus}
-  <div
-    class="overlay {$overlayStatus ? 'overlay-visible' : ''}"
-    on:click={$overlayStatus ? overlayStatus.set(false) : null}
-    tabindex="0"
-    role="button"
-    aria-label="Sluit overlay"
-    style="top: var(--navbar-height);">
-    <!-- Overlay content weergave blijft hetzelfde -->
+  <div class="overlay {$overlayStatus ? 'overlay-visible' : ''}" on:click={(event) => overlayStatus.set(false)} on:keydown={(event) => event.key === "Escape" && overlayStatus.set(false)} tabindex="0" role="button" aria-label="Sluit overlay" style="top: var(--navbar-height);">
+    {#if $overlayContent === "home"}
+      <HomeOverlay />
+    {:else if $overlayContent === "herontworpen"}
+      <HerontworpenOverlay />
+    {:else if $overlayContent === "opvallende"}
+      <OpvallendeOverlay />
+    {:else if $overlayContent === "herontwerpvri"}
+      <HerontwerpvriOverlay /> <!-- Zorg ervoor dat deze conditional correct is -->
+    {/if}
   </div>
 {/if}
 
